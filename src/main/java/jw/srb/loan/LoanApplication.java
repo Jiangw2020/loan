a8b27216a002bcc717335144a5bcf4dd1d093a12
jiangwei <734145091@qq.com>
2021-03-31 09:01:02 +0800

    Initial commit

---
+++ src/main/java/jw/srb/loan/LoanApplication.java
@@ -0,0 +1,13 @@
+package jw.srb.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jw/srb/loan/LoanApplicationTests.java
@@ -0,0 +1,13 @@
+package jw.srb.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
